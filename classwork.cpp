159cb4e14bf88873f30ba37bb2a1eff286c3fa92
lanantran <lntran114@student.hbuhsd.edu>
2024-12-11 20:57:37 +0000

    new code

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,10 @@
 
 int main()
 {
-  // write code here
+  int age = 10;
+  double price = 19.99; 
+  bool isRaining = false;
   
+  std::cout << age << "/n" << price << std::endl << isRaining;
   return 0;
 }

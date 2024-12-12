54d985e03f844b87bdca74cce9404a723a54f8cd
lanantran <lntran114@student.hbuhsd.edu>
2024-12-12 21:46:43 +0000

    assignment 4.1 start

--- classwork.cpp
+++ classwork.cpp
@@ -13,3 +13,49 @@ int main()
   std::cout << "I am a" << grade_level << "th grader." << std::end1; 
    return 0;
 }
+
+#include <iostream>
+#include <string> 
+
+int main()
+{
+  int num = 5; 
+  num = 10;
+
+  std::string stu_name = "John"; 
+  std:: string number = "5"
+
+  std::cout << "My name is" << stu_name << std::endl;
+
+  return 0;
+}
+
+
+
+Assignment 4.1 
+
+#include <iostream> 
+#include <string> 
+// Exercise 1 
+{
+ int x = 25; 
+ int y = 3; 
+ int add_xy = x + y;
+ int sub_xy = x - y; 
+ int mult_xy = x * y; 
+ int div_xy = x / y; 
+ int mod_xy = x % y;  
+  
+std::cout << add_xy << std::endl
+std::cout << sub_xy <<
+
+}
+
+
+
+// Exercise 2 
+bool isSunny = true; 
+bool has Umbrella = false; 
+std::cout << (isSunny && hasUmbrella) << std::endl; 
+
+bool sun_or_umb = isSunny || hasUmbrella; 
